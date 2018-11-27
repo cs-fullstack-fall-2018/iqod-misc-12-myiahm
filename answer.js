0acd7a08fef94f2d6337e01e42bd315bc0f51f24
Christopher Pace <Christopher_pace@ymail.com>
2018-11-27 10:06:16 -0600

    check in

--- answer.js
+++ answer.js
@@ -1,25 +1,22 @@
-multiArray = [["Hello", " Kevin", " and Erin"],["Hi", " Kenn"],["Good", " morning", " Meka"]];
+multiArray = [["Hello", " Kevin", " and Erin"], ["Hi", " Kenn"], ["Good", " morning", " Meka"]];
 
 
 var array1 = multiArray[0];
 var array2 = multiArray[1];
 var array3 = multiArray[2];
 
-for(i=0;i<multiArray.length;i++) {
-    var newa = multiArray[i]+"";
-    // for (x=0;x<newa.length;x++)
-    var newArray = [];
-    newArray.push(newa);
+for (i = 0; i < multiArray.length; i++) {
+    var newa = multiArray+"";
+    for (x = 0; x < newa.length; x++)
+         var newArray = [];
+        newArray.push();
 
-    console.log(newa)
+        console.log(newArray)
 
 }
 
 
-
-
-
-console.log("'"+array1[0],array1[1],array1[2]+"'");
+console.log("'" + array1[0], array1[1], array1[2] + "'");
 // newArray = iqod12(multiArray)
 //
 // newArray is ["Hello Kevin and Erin", "Hi Kenn", "Good morning Meka"]
